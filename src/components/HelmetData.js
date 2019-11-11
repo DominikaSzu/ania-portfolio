b18d09fa0baffee66651ba1b5cbad98550cc0088
DominikaSzu <dominika.szukala@gmail.com>
2019-11-11 12:42:31 +0100

    rwd fix

--- src/components/HelmetData.js
+++ src/components/HelmetData.js
@@ -5,6 +5,8 @@ const HelmetData = () => {
     return (
         <Helmet>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>Anna Szukała - Art & Illustration</title>
+            <meta data-react-helmet="true" http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         </Helmet>
     )
 }
